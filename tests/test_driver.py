342b888dcd3902735c8929dfc82cecd1beb30a00
PUI LENG CHIN <82777253+bellechin77@users.noreply.github.com>
2025-04-01 12:33:28 +0200

    Delete tests/test_driver.py

--- tests/test_driver.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-driver.get("https://www.google.com")
-print("✅ WebDriver launched successfully!")
-driver.quit()
